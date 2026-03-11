4195896fbc532a1a2d9c155188d2c2b0ef96e4a3
Neela-lavanya <lavanyaneela1593@gmail.com>
2026-03-11 19:48:37 +0530

    Add files via upload

---
+++ Week4/DAY18/DAY18 task4-program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int evenCount = 0;
+            int oddCount = 0;
+            int sum = 0;
+
+            Console.WriteLine("Enter Number: ");
+            number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                sum = sum + i;
+
+                if (i % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine("*** Number Analysis ***");
+            Console.WriteLine($"Even Count: {evenCount}");
+            Console.WriteLine($"Odd Count: {oddCount}");
+            Console.WriteLine($"Sum: {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
