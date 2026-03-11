1c07ac76f11decb8db79a622d3e2dba46f026c44
Neela-lavanya <lavanyaneela1593@gmail.com>
2026-03-11 19:51:17 +0530

    Add files via upload

---
+++ Week4/DAY18/DAY18task3-program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    internal class Program
+    {
+        static double CalculateBonus(double salary, int experience)
+        {
+            double bonusPercentage;
+
+            if (experience < 2)
+            {
+                bonusPercentage = 0.05;
+            }
+            else if (experience >= 2 && experience <= 5)
+            {
+                bonusPercentage = 0.10;
+            }
+            else
+            {
+                bonusPercentage = 0.15;
+            }
+
+            double bonus = salary * bonusPercentage;
+            return bonus;
+        }
+
+        static void Main(string[] args)
+        {
+            string name;
+            double salary;
+            int experience;
+            double bonus;
+            double finalSalary;
+
+            Console.WriteLine("Enter Name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter Salary: ");
+            salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Experience (years): ");
+            experience = int.Parse(Console.ReadLine());
+
+            bonus = CalculateBonus(salary, experience);
+
+            
+            finalSalary = bonus > 0 ? salary + bonus : salary;
+
+            Console.WriteLine("****** Employee Details ******");
+            Console.WriteLine($"Employee: {name}");
+            Console.WriteLine($"Bonus: {bonus}");
+            Console.WriteLine($"Final Salary: {finalSalary}");
+
+            Console.ReadLine();
+        }
+    }
+}
