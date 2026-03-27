2bc5b1be091bdea6f678cb60b8ede74d63e5f1c4
Neela-lavanya <lavanyaneela1593@gmail.com>
2026-03-27 14:32:38 +0530

    Add files via upload

---
+++ Week6/DAY27/Problem5-singleton/ConfigurationManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp12
+{
+    public class ConfigurationManager
+    {
+        private static ConfigurationManager instance;
+
+        public string AppName { get; set; }
+
+        private ConfigurationManager()
+        {
+            AppName = "MyApp";
+        }
+
+        public static ConfigurationManager GetInstance()
+        {
+            if (instance == null)
+                instance = new ConfigurationManager();
+
+            return instance;
+        }
+    }
+}
---
+++ Week6/DAY27/Problem5-singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp12;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+// Entity
+class Program
+{
+    static void Main()
+    {
+        var obj1 = ConfigurationManager.GetInstance();
+        var obj2 = ConfigurationManager.GetInstance();
+
+        Console.WriteLine(obj1.AppName);
+        Console.WriteLine(object.ReferenceEquals(obj1, obj2)); // True
+    }
+}
