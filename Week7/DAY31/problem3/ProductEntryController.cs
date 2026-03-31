4f662a9d0b5e487922349a88eb73e65b1dcbc0a9
Neela-lavanya <lavanyaneela1593@gmail.com>
2026-03-31 18:20:30 +0530

    Add files via upload

---
+++ Week7/DAY31/problem3/ProductEntryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("product")]
+    public class ProductEntryController : Controller
+    {
+        
+        private static List<object> productList = new List<object>();
+
+        
+        [HttpGet("entry")]
+        public IActionResult Entry()
+        {
+            ViewBag.Products = productList;
+            return View();
+        }
+
+     
+        [HttpPost("entry")]
+        public IActionResult Entry(string name, decimal price, int quantity)
+        {
+          
+            var product = new
+            {
+                Name = name,
+                Price = price,
+                Quantity = quantity
+            };
+
+            // Add to list
+            productList.Add(product);
+
+            // Send list to view
+            ViewBag.Products = productList;
+
+            return View();
+        }
+    }
+}
