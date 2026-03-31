f59d7efb57b0d4f221e0a4756600c70fa4ecb399
Neela-lavanya <lavanyaneela1593@gmail.com>
2026-03-31 18:21:47 +0530

    Add files via upload

---
+++ Week7/DAY31/problem4/FeedbackController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    namespace WebApplication2.Controllers
+    {
+        [Route("feedback")]
+        public class FeedbackController : Controller
+        {
+            
+            [HttpGet("form")]
+            public IActionResult Form()
+            {
+                return View();
+            }
+
+            [HttpPost("form")]
+            public IActionResult Form(string name, string comments, int rating)
+            {
+               
+                if (rating >= 4)
+                {
+                    ViewData["Message"] = "Thank You for your positive feedback!";
+                }
+                else
+                {
+                    ViewData["Message"] = "We will improve based on your feedback.";
+                }
+
+                return View();
+            }
+        }
+    }
+}
