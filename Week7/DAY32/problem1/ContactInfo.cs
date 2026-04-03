54128d2bf23cab705abcbf0d8d1d69c6eb13d9cc
Neela-lavanya <lavanyaneela1593@gmail.com>
2026-04-03 15:24:10 +0530

    Add files via upload

---
+++ Week7/DAY32/problem1/ContactInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class ContactInfo
+    {
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Company Name is required")]
+        public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Enter valid Email")]
+        public string EmailId { get; set; }
+        
+
+        [Required(ErrorMessage = "Mobile Number is required")]
+        [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Enter valid 10-digit Mobile Number")]
+        public string MobileNo { get; set; }
+        [Required(ErrorMessage = "Designation is required")]
+        public string Designation { get; set; }
+    }
+}
