0e62516c5c873cd677b91b26a8ec6867e1a248e6
Neela-lavanya <lavanyaneela1593@gmail.com>
2026-04-03 15:23:34 +0530

    Add files via upload

---
+++ Week7/DAY32/problem1/ContactController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using WebApplication2.Services;
+
+namespace WebApplication2.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _service;
+
+        // Constructor Injection
+        public ContactController(IContactService service)
+        {
+            _service = service;
+        }
+
+        // Show all contacts
+        public IActionResult ShowContacts()
+        {
+            var list = _service.GetAllContacts();
+            return View(list);
+        }
+
+        // Search by ID
+        public IActionResult GetContactById(int id)
+        {
+            var contact = _service.GetContactById(id);
+            return View(contact);
+        }
+
+        // GET Add
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+
+        // POST Add
+        [HttpPost]
+        public IActionResult AddContact(ContactInfo contact)
+        {
+            _service.AddContact(contact);
+            return RedirectToAction("ShowContacts");
+        }
+    }
+}
---
+++ Week7/DAY32/problem1/ContactService.cs
@@ -0,0 +1,25 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Services
+{
+    public class ContactService : IContactService
+    {
+        private static List<ContactInfo> contacts = new List<ContactInfo>();
+
+        public List<ContactInfo> GetAllContacts()
+        {
+            return contacts;
+        }
+
+        public ContactInfo GetContactById(int id)
+        {
+            return contacts.FirstOrDefault(c => c.ContactId == id);
+        }
+
+        public void AddContact(ContactInfo contact)
+        {
+            contact.ContactId = contacts.Count + 1; 
+            contacts.Add(contact);
+        }
+    }
+}
