f90697caa403603617080c92c957ca3333783fbe
Neela-lavanya <lavanyaneela1593@gmail.com>
2026-04-16 11:16:40 +0530

    Add files via upload

---
+++ Week9/DAY42/ContactService/Repositories/ContactRepository.cs
@@ -0,0 +1,46 @@
+﻿using ContactService.Data;
+using ContactService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactService.Repositories
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly ContactDbContext _context;
+
+        public ContactRepository(ContactDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Contact>> GetAll()
+            => await _context.Contacts.ToListAsync();
+
+        public async Task<Contact?> GetById(int id)
+            => await _context.Contacts.FindAsync(id);
+
+        public async Task<Contact> Add(Contact contact)
+        {
+            _context.Contacts.Add(contact);
+            await _context.SaveChangesAsync();
+            return contact;
+        }
+
+        public async Task<Contact> Update(Contact contact)
+        {
+            _context.Contacts.Update(contact);
+            await _context.SaveChangesAsync();
+            return contact;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var data = await _context.Contacts.FindAsync(id);
+            if (data == null) return false;
+
+            _context.Contacts.Remove(data);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ Week9/DAY42/ContactService/Services/IContactService.cs
@@ -0,0 +1,13 @@
+﻿using ContactService.Models;
+
+namespace ContactService.Services
+{
+    public interface IContactService
+    {
+        Task<List<Contact>> GetAll();
+        Task<Contact?> GetById(int id);
+        Task<Contact> Add(Contact contact);
+        Task<Contact> Update(Contact contact);
+        Task<bool> Delete(int id);
+    }
+}
