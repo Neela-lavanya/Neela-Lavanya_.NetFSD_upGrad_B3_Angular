a1233a30df77f2bbece1ce58741303e2c9182ba4
Neela-lavanya <lavanyaneela1593@gmail.com>
2026-04-16 10:58:55 +0530

    Add files via upload

---
+++ Week9/DAY42/ContactService/ContactsController.cs
@@ -0,0 +1,38 @@
+﻿using ContactService.Models;
+using ContactService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactService _service;
+
+        public ContactsController(IContactService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+            => Ok(await _service.GetAll());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+            => Ok(await _service.GetById(id));
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Contact contact)
+            => Ok(await _service.Add(contact));
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Contact contact)
+            => Ok(await _service.Update(contact));
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+            => Ok(await _service.Delete(id));
+    }
+}
